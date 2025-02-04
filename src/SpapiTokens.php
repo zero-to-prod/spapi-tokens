f603068c9554b98f347138fa487e0fd9dcfe87df
david_smith <david_smith@sweetwater.com>
2025-02-03 20:01:03 -0500

    main Updated `src/SpapiTokens.php`.

--- src/SpapiTokens.php
+++ src/SpapiTokens.php
@@ -23,52 +23,65 @@ class SpapiTokens
      * @param  string       $url                The URL for the api
      *
      * @return array{
-     *     info: array{
-     *         url: string,
-     *         content_type: string,
-     *         http_code: int,
-     *         header_size: int,
-     *         request_size: int,
-     *         filetime: int,
-     *         ssl_verify_result: int,
-     *         redirect_count: int,
-     *         total_time: float,
-     *         namelookup_time: float,
-     *         connect_time: float,
-     *         pretransfer_time: float,
-     *         size_upload: int,
-     *         size_download: int,
-     *         speed_download: int,
-     *         speed_upload: int,
-     *         download_content_length: int,
-     *         upload_content_length: int,
-     *         starttransfer_time: float,
-     *         redirect_time: float,
-     *         redirect_url: string,
-     *         primary_ip: string,
-     *         certinfo: array,
-     *         primary_port: int,
-     *         local_ip: string,
-     *         local_port: int,
-     *         http_version: int,
-     *         protocol: int,
-     *         ssl_verifyresult: int,
-     *         scheme: string,
-     *         appconnect_time_us: int,
-     *         connect_time_us: int,
-     *         namelookup_time_us: int,
-     *         pretransfer_time_us: int,
-     *         redirect_time_us: int,
-     *         starttransfer_time_us: int,
-     *         total_time_us: int
-     *     },
-     *     error: string,
-     *     response: array{
-     *         expiresIn: int,
-     *         restrictedDataToken: string
-     *     }
-     * }
-     *
+     *  info: array{
+     *      url: string,
+     *      content_type: string,
+     *      http_code: int,
+     *      header_size: int,
+     *      request_size: int,
+     *      filetime: int,
+     *      ssl_verify_result: int,
+     *      redirect_count: int,
+     *      total_time: float,
+     *      namelookup_time: float,
+     *      connect_time: float,
+     *      pretransfer_time: float,
+     *      size_upload: int,
+     *      size_download: int,
+     *      speed_download: int,
+     *      speed_upload: int,
+     *      download_content_length: int,
+     *      upload_content_length: int,
+     *      starttransfer_time: float,
+     *      redirect_time: float,
+     *      redirect_url: string,
+     *      primary_ip: string,
+     *      certinfo: array,
+     *      primary_port: int,
+     *      local_ip: string,
+     *      local_port: int,
+     *      http_version: int,
+     *      protocol: int,
+     *      ssl_verifyresult: int,
+     *      scheme: string,
+     *      appconnect_time_us: int,
+     *      connect_time_us: int,
+     *      namelookup_time_us: int,
+     *      pretransfer_time_us: int,
+     *      redirect_time_us: int,
+     *      starttransfer_time_us: int,
+     *      total_time_us: int
+     *  },
+     *  error: string,
+     *  headers: array{
+     *      Server: string,
+     *      Date: string,
+     *      Content-Type: string,
+     *      Content-Length: string,
+     *      Connection: string,
+     *      'x-amz-rid': string,
+     *      'x-amzn-RateLimit-Limit': string,
+     *      'x-amzn-RequestId': string,
+     *      'x-amz-apigw-id': string,
+     *      'X-Amzn-Trace-Id': string,
+     *      Vary: string,
+     *      'Strict-Transport-Security': string
+     *  },
+     *  response: array{
+     *      expiresIn: int,
+     *      restrictedDataToken: string
+     *  }
+     *     *
      * @link https://developer-docs.amazon.com/sp-api/docs/tokens-api-v2021-03-01-reference
      */
     public static function restrictedDataToken(string $access_token, string $path, array $dataElements = [], ?string $targetApplication = null, ?string $user_agent = null, string $url = 'https://sellingpartnerapi-na.amazon.com/tokens/2021-03-01/restrictedDataToken'): array
